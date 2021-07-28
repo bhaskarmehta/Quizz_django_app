2e2257c01ab8f1292f96a5265729b182a18ddc51
Bhaskar mehta <bhaskarmehta422@gmail.com>
2021-07-29 01:38:00 +0530

    added comment in views.py

--- quizz/views.py
+++ quizz/views.py
@@ -12,6 +12,7 @@ from quizz.models import Quizz # if we had more than one model we could have giv
   #  val2=int(request.POST['num2'])
    # res=(val1)+(val2)
     #return render(request,'addition_result.html',{'result':res})
+    #comment for testing git commit
 
 
 def home(request):
